f0d9181eba98228073bc8b425c74f459b970f60f
DipaShaha <dipashaha92@gmail.com>
2019-05-16 23:19:44 +0600

    commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,19 @@
-index.js
+
+
+const songsReducers=()=>{
+	return[
+		{title:No Scrubs, duration :'2:30'},
+		{title:All star, duration :'3:20'},
+		{title:No pain, duration :'2:55'},
+		{title:nothing , duration :'4:30'}
+
+	];
+};
+
+const selectedSongReducer=(selectedSong=null,action)=>{
+	if(action.type=='SONG_SELECTED')
+		return action.payload;
+	return selectedSong;
+}
+
+
